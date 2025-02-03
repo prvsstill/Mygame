ca693bdf19606770c1ab35179608d443cf973196
prvsstill <87117243+prvsstill@users.noreply.github.com>
2025-02-04 00:15:22 +0800

    移动改动
    - 新增了移动属性和方法
    - 可以实现连续移动

--- player_ship.py
+++ player_ship.py
@@ -19,10 +19,33 @@ class Ship:
 
         self.screen = screen
         self.screen_rect = screen.get_rect()
-        self.image = pygame.image.load("images/古神.png")
+
+        original_image = pygame.image.load("images/古神.png")
+        self.image = pygame.transform.scale(
+            original_image, (50, 50)
+        )
         self.rect = self.image.get_rect()
 
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # 移动标志
+        self.moving_up = False
+        self.moving_down = False
+        self.moving_left = False
+        self.moving_right = False
+        self.speed = 4
+
+        self.dead = False
+
     def place_ship(self):
         self.screen.blit(self.image, self.rect)
+
+    def move_ship(self):
+        if self.moving_up and self.rect.top > 0:
+            self.rect.y -= self.speed
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.rect.y += self.speed
+        if self.moving_left and self.rect.left > 0:
+            self.rect.x -= self.speed
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.rect.x += self.speed
